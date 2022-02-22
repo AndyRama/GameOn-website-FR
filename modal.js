93538ee10aa006ca17649aef5ebe8e6a63f2e8b0
AndyRama <andyramaroson@gmail.com>
2022-02-22 16:43:47 +0100

    [enhancement] validation for name

--- modal.js
+++ modal.js
@@ -13,6 +13,11 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelectorAll(".close");
 
+const formModal = document.getElementById("form-modal")
+const firstName = document.getElementById("first");
+
+const errorFirst = document.querySelector(".errorFirst")
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -25,5 +30,35 @@ function launchModal() {
 document.getElementById("closemodal").addEventListener("click", function(e) {
   modalbg.style.display ="none";
 })
+
+//Check validation for name[nb letters] < 2
+function checkFirstName(value) {
+  if(value && value.length > 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// Reset error 
+function resetError() {
+  errorFirst.innerHTML = "";
+}
+
+//listen activity for form
+formModal.addEventListener("submit", event => {
+  event.preventDefault();
+  resetError();
+  if(!checkFirstName(firstName.value)){
+    errorFirst.innerHTML = "Veuillez entrer 2 caractères ou plus pour votre prénom.";
+    errorFirst.style.color = "red";
+    errorFirst.style.fontSize = "0.8rem";
+  } else {
+    errorFirst.innerHTML =" Votre prénom est valide";
+    errorFirst.style.color = "green";
+    errorFirst.style.fontSize= "0.8rem";
+  }
+});
 
 
